4f63cf5a4cd34ecfebc01dcfbc3e67d74caeba1c
gabor520 <pinielgabor1@gmail.com>
2023-02-09 12:00:20 +0100

    folytatas

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ def pontokosszege(lista):
 def tesztek(jatekos_vesztett_teszt):
   lista = [22,23,24]
   jatekos_vesztett_teszt = lista
+
+def jatekos_vesztett_teszt():
+    jatekoslapok = [10,9,3]
+    geplapok = [10,11]
+    print(eredmeny(jatekos,gep))
+
+
+def gep_vesztett_teszt():
+    jatekoslapok = [10,11]
+    geplapok = [10,9,3]
+
+
+
+def tesztek(gep_vesztett_teszt):
+    lista = [22,23,24]
+    gep_vesztett_teszt = lista
